69f89d516cfd1013ec293384fd1e57840214a3e9
firmannuzul <firmann48@gmail.com>
2025-12-01 21:16:10 +0700

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { Express } from "express";
+import cors from "cors";
+import { PORT } from "./config/env";
+import { SampleRouter } from "./modules/sample/sample.router";
+import { errorMiddleware } from "./middlewares/error.middleware";
+
+export class App {
+  app: Express;
+
+  constructor() {
+    this.app = express();
+    this.configure();
+    this.routes();
+    this.handleError();
+  }
+
+  private configure() {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  private routes() {
+    const sampleRouter = new SampleRouter();
+    this.app.use("/samples", sampleRouter.getRouter());
+  }
+
+  private handleError() {
+    this.app.use(errorMiddleware);
+  }
+
+  public start() {
+    this.app.listen(PORT, () => {
+      console.log(`server running at port: ${PORT}`);
+    });
+  }
+}
---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { ApiError } from "../utils/api-error";
+
+export const errorMiddleware = (
+  err: ApiError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const message = err.message || "something went wrong";
+  const status = err.status || 500;
+
+  res.status(status).send({ message });
+};
